42fd61dbb88c337f9fb5daf8f966e673e75da8cb
Samantha <samvel200355@gmail.com>
2024-11-04 22:10:33 -0600

    Inserts de AdmissionDegree y endpoint para la vista de admisiones

--- db-scripts/ddl/create.sql
+++ db-scripts/ddl/create.sql
@@ -13,18 +13,18 @@ CREATE TABLE RegionalCenter(
 
 CREATE TABLE AdmissionTest(
 	id TINYINT PRIMARY KEY AUTO_INCREMENT,
-    description VARCHAR(30) NOT NULL
+    description VARCHAR(62) NOT NULL
     
 );
 
 CREATE TABLE DegreeProgram(
 	id SMALLINT PRIMARY KEY AUTO_INCREMENT,
-    description VARCHAR (50) NOT NULL
+    description VARCHAR (60) NOT NULL
 );
 
 CREATE TABLE AdmissionDegree(
 	id SMALLINT PRIMARY KEY AUTO_INCREMENT,
-    description VARCHAR (30) NOT NULL,
+    description VARCHAR (100) NOT NULL,
     degree SMALLINT,
     admissionTest TINYINT,
 	passingGrade SMALLINT,
@@ -75,12 +75,12 @@ CREATE TABLE Employee(
 
 CREATE TABLE ProfessorType(
 	id TINYINT PRIMARY KEY AUTO_INCREMENT,
-    description VARCHAR(20)
+    description VARCHAR(30)
 );
 
 CREATE TABLE Department(
 	id SMALLINT PRIMARY KEY AUTO_INCREMENT,
-    description VARCHAR(20)
+    description VARCHAR(60)
 );
 
 CREATE TABLE Professor(
@@ -105,6 +105,7 @@ CREATE TABLE Administrative(
 );
 
 
+
 INSERT INTO RegionalCenter(description, location) VALUES
     ('Ciudad Universitaria (CU)','Tegucigalpa, Francisco Morazán'),
     ('Centro Universitario Regional del Norte (CURN)','San Pedro Sula, Cortés'),
@@ -121,55 +122,128 @@ INSERT INTO AdministrativeType(description) VALUES
     ('DIPP')
 ;
 
-INSERT INTO DegreeProgram(description) VALUES
-    ('Licenciatura en Administración de Empresas'),
-    ('Licenciatura en Contaduría Pública'),
-    ('Licenciatura en Mercadotecnia'),
-    ('Licenciatura en Recursos Humanos'),
-    ('Licenciatura en Economía'),
-    ('Licenciatura en Desarrollo Internacional'),
-    ('Licenciatura en Sociología'),
-    ('Licenciatura en Trabajo Social'),
-    ('Licenciatura en Psicología'),
-    ('Licenciatura en Comunicación Social'),
-    ('Licenciatura en Ciencias Jurídicas'),
-    ('Licenciatura en Notariado y Registro'),
-    ('Licenciatura en Ingeniería Civil'),
-    ('Licenciatura en Ingeniería Industrial'),
-    ('Licenciatura en Ingeniería en Sistemas'),
-    ('Licenciatura en Ingeniería Electrónica'),
-    ('Licenciatura en Ingeniería Agronómica'),
-    ('Licenciatura en Medicina'),
-    ('Licenciatura en Enfermería'),
-    ('Licenciatura en Odontología'),
-    ('Licenciatura en Nutrición'),
-    ('Licenciatura en Farmacia'),
-    ('Licenciatura en Biología'),
-    ('Licenciatura en Química'),
-    ('Licenciatura en Física'),
-    ('Licenciatura en Matemáticas'),
-    ('Licenciatura en Tecnología de Alimentos'),
-    ('Licenciatura en Artes Plásticas'),
-    ('Licenciatura en Música'),
-    ('Licenciatura en Danza'),
-    ('Licenciatura en Teología'),
-    ('Licenciatura en Educación Teológica'),
-    ('Licenciatura en Agronomía'),
-    ('Licenciatura en Zootecnia'),
-    ('Licenciatura en Medicina Veterinaria'),
-    ('Licenciatura en Educación Primaria'),
-    ('Licenciatura en Educación Secundaria'),
-    ('Licenciatura en Psicopedagogía'),
-    ('Licenciatura en Enseñanza de Inglés'),
-    ('Licenciatura en Traducción e Interpretación'),
-    ('Licenciatura en Periodismo'),
-    ('Licenciatura en Publicidad y Relaciones Públicas'),
-    ('Licenciatura en Filosofía'),
-    ('Licenciatura en Literatura')
+INSERT INTO DegreeProgram (description) VALUES 
+    ('Derecho'),
+    ('Antropología'),
+    ('Periodismo'),
+    ('Psicología'),
+    ('Pedagogía'),
+    ('Trabajo Social'),
+    ('Historia'),
+    ('Letras'),
+    ('Filosofía'),
+    ('Sociología'),
+    ('Educación Física'),
+    ('Lenguas Extranjeras'),
+    ('Música'),
+    ('Desarrollo Local'),
+    ('Ingeniería Civil'),
+    ('Ingeniería Mecánica '),
+    ('Ingeniería Eléctrica '),
+    ('Ingeniería Industrial'),
+    ('Ingeniería en Sistemas'),
+    ('Arquitectura'),
+    ('Matemáticas'),
+    ('Física'),
+    ('Astronomía y Astrofísica'),
+    ('Ciencia y Tecnologías de la Información'),
+    ('Medicina'),
+    ('Odontología'),
+    ('Nutrición'),
+    ('Química y Farmacia'),
+    ('Enfermería'),
+    ('Microbiología'),
+    ('Biología'),
+    ('Administración de Empresas'),
+    ('Administración Pública'),
+    ('Economía'),
+    ('Contaduría Pública y Finanzas'),
+    ('Administración Aduanera'),
+    ('Administración de Banca y Finanzas'),
+    ('Comercio Internacional'),
+    ('Informática Administrativa'),
+    ('Mercadotecnia'),
+    ('Ingeniería Agronómica'),
+    ('Ingeniería Forestal'),
+    ('Ingeniería Agroindustrial'),
+    ('Ingeniería en Ciencias Acuícolas y Recursos Marinos Costeros'),
+    ('Economía Agrícola'),
+    ('Comercio Internacional con Orientación en Agroindustria'),
+    ('Administración de Empresas Agropecuarias'),
+    ('Ecoturismo'),
+    ('Ingeniería Química'),
+    ('Química Industrial'),
+    ('Geología')
 ;
 
 INSERT INTO ProfessorType(description) VALUES
-    ('Docente'),
+    ('Docente por hora'),
+    ('Docente Permanente'),
     ('Coordinador'),
     ('Jefe de departamento')
+;
+
+INSERT INTO AdmissionTest (description) VALUES 
+    ('Prueba de Aptitud Académica'),
+    ('Prueba de Conocimientos para Ciencias Naturales y de la Salud')
+;
+
+INSERT INTO AdmissionDegree (description, degree, admissionTest, passingGrade) VALUES 
+    ('PAA para Derecho', 1, 1, 700),
+    ('PAA para Antropología', 2, 1, 700),
+    ('PAA para Periodismo', 3, 1, 700),
+    ('PAA para Psicología', 4, 1, 700),
+    ('PAA para Pedagogía', 5, 1, 700),
+    ('PAA para Trabajo Social', 6, 1, 700),
+    ('PAA para Historia', 7, 1, 700),
+    ('PAA para Letras', 8, 1, 700),
+    ('PAA para Filosofía', 9, 1, 700),
+    ('PAA para Sociología', 10, 1, 700),
+    ('PAA para Educación Física', 11, 1, 700),
+    ('PAA para Lenguas Extranjeras', 12, 1, 700),
+    ('PAA para Música', 13, 1, 700),
+    ('PAA para Desarrollo Local', 14, 1, 700),
+    ('PAA para Ingeniería Civil', 15, 1, 1000),
+    ('PAA para Ingeniería Mecánica Industrial', 16, 1, 1000),
+    ('PAA para Ingeniería Eléctrica Industrial', 17, 1, 1000),
+    ('PAA para Ingeniería Industrial', 18, 1, 1000),
+    ('PAA para Ingeniería en Sistemas', 19, 1, 1000),
+    ('PAA para Arquitectura', 20, 1, 700),
+    ('PAA para Matemáticas', 21, 1, 700),
+    ('PAA para Física', 22, 1, 700),
+    ('PAA para Astronomía y Astrofísica', 23, 1, 700),
+    ('PAA para Ciencia y Tecnologías de la Información Geográfica', 24, 1, 700),
+    ('PAA para Medicina', 25, 1, 1100),
+    ('PAA para Odontología', 26, 1, 900),
+    ('PAA para Nutrición', 27, 1, 900),
+    ('PAA para Química y Farmacia', 28, 1, 900),
+    ('PAA para Enfermería', 29, 1, 900),
+    ('PAA para Microbiología', 30, 1, 900),
+    ('PAA para Biología', 31, 1, 700),
+    ('PAA para Administración de Empresas', 32, 1, 700),
+    ('PAA para Administración Pública', 33, 1, 700),
+    ('PAA para Economía', 34, 1, 700),
+    ('PAA para Contaduría Pública y Finanzas', 35, 1, 700),
+    ('PAA para Administración Aduanera', 36, 1, 700),
+    ('PAA para Administración de Banca y Finanzas', 37, 1, 700),
+    ('PAA para Comercio Internacional', 38, 1, 700),
+    ('PAA para Informática Administrativa', 39, 1, 700),
+    ('PAA para Mercadotecnia', 40, 1, 700),
+    ('PAA para Ingeniería Agronómica', 41, 1, 1000),
+    ('PAA para Ingeniería Forestal', 42, 1, 1000),
+    ('PAA para Ingeniería Agroindustrial', 43, 1, 1000),
+    ('PAA para Ingeniería en Ciencias Acuícolas', 44, 1, 1000),
+    ('PAA para Economía Agrícola', 45, 1, 700),
+    ('PAA para Comercio Internacional con Orientación en Agroindustria', 46, 1, 700),
+    ('PAA para Administración de Empresas Agropecuarias', 47, 1, 700),
+    ('PAA para Ecoturismo', 48, 1, 700),
+    ('PAA para Ingeniería Química', 49, 1, 1000),
+    ('PAA para Química Industrial', 50, 1, 700),
+    ('PAA para Geología', 51, 1, 700),
+    ('PCCNS para Medicina', 25, 2, 400),
+    ('PCCNS para Odontología', 26, 2, 400),
+    ('PCCNS para Nutrición', 27, 2, 400),
+    ('PCCNS para Química y Farmacia', 28, 2, 400),
+    ('PCCNS para Enfermería', 29, 2, 400),
+    ('PCCNS para Microbiología', 30, 2, 400)
 ;
---
+++ public/api/get/infoAdmission/index.php
@@ -0,0 +1,24 @@
+<?php
+
+    header("Content-Type: application/json");
+
+    include_once "../../../../src/GenericGet/GenericGet.php";
+
+    //Data Access Object
+    $dao = new GenericGetDAO("localhost", "is", "is", "ProyectoIS");
+    $degrees = $dao->getDegrees();
+    $centers = $dao->getCenters();
+
+    $json = [
+        "message"=> "Peticion realizada con exito",
+        "status"=> true,
+        "data"=> [
+            "careers" => $degrees,
+            "regionalCenters" => $centers,
+        ]
+            
+    ];
+    
+    echo json_encode($json);
+
+?>
