6594f76fe61a7c27f4815869af6fd4571611aeb3
angelcastillo25 <149858015+angelcastillo25@users.noreply.github.com>
2024-11-09 00:09:05 -0600

    Se implemento modal que responde en funcion de la respuesta del servidor al mandar peticion post

--- public/assets/js/modules/Popup.js
+++ public/assets/js/modules/Popup.js
@@ -2,27 +2,32 @@
  * Esta clase abre el componente pop dependiendo de la respuesta de backend
  * 
  * author: afcastillof@unah.hn
- * version: 0.1.1
- * date: 8/11/24
+ * version: 0.1.0
+ * date: 9/11/24
  */
 export class Popup {
   
     /**
      * Abre el componente pop
-     * @param {string} url 
-     * @returns {Object|null} 
-     * @throws {Error} 
+     * @param {div} contenedor de pop
      */
-    static open(popup, result) {
-        console.log(result)
-        if(result.status){
+    static open(popup) {
             popup.style.display = "block";
             window.scrollTo({ top: 0, behavior: "smooth" });
-        }
         
     }
-   
-      
-    
+
+    //Cierra el componente y redirige a la pantalla de landingPage
+
+    static close1(popup) {
+        popup.style.display = "none";
+        window.location.href = '/';
+    }
+
+    //Cierra el componente
+    static close(popup) {
+        popup.style.display = "none";
+    }
+        
   }
   
