cfc0c43375d4a43b7b765dfc2336d1ebfc02d6cb
dan8a112 <danyochoa112@gmail.com>
2024-11-08 23:40:05 -0600

    Se hace el envio del formulario del login y validacion de respuesta

---
+++ public/assets/js/loginSEDP/Action.js
@@ -0,0 +1,30 @@
+import { HttpRequest } from "../modules/HttpRequest.js";
+
+class Action {
+
+    static async submitLoginForm(event){
+
+        event.preventDefault();
+
+        //Se envia el formulario con correo y contraseña
+        const data = await HttpRequest.submitForm(event,"http://localhost:3000/api/post/loginSEDP");
+
+        //Se evalua la respuesta
+        if (data.status) {
+            //redirecciona a pagina principal
+            window.location.href = "../administration/sedp-portal.html"
+        
+        }else{
+            const errorSection = document.querySelector("#errorSection");
+
+            const errorText = document.createElement("p");
+            //Se genera un texto de error
+            errorText.innerText =  "El correo o la contraseña es incorrecto, vuelva a intentarlo."
+            errorText.style.color = "red";
+            errorSection.appendChild(errorText);
+        }
+    }
+
+}
+
+export {Action}
