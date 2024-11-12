906eeb877f083790b0b89ac3f8b51cad3b79bf93
Samantha <samvel200355@gmail.com>
2024-11-11 22:42:02 -0600

    actualizacion ddl

--- db-scripts/ddl/create.sql
+++ db-scripts/ddl/create.sql
@@ -65,6 +65,19 @@ CREATE TABLE AcademicEvent(
     CONSTRAINT fk_process FOREIGN KEY(process) REFERENCES AcademicProcess(id)
 );
 
+CREATE TABLE AcademicSubprocess (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    academicEventId INT NOT NULL,
+    academicProcessId INT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(255),
+    startDate DATETIME,
+    endDate DATETIME,
+    active TINYINT(1) DEFAULT 1,
+    FOREIGN KEY (academicEventId) REFERENCES AcademicEvent(id),
+    FOREIGN KEY (academicProcessId) REFERENCES AcademicProcess(id)
+);
+
 CREATE TABLE Application(
 	id INT PRIMARY KEY AUTO_INCREMENT,
     idApplicant VARCHAR(15),
@@ -142,26 +155,26 @@ CREATE TABLE Configuration(
     data json
 );
 
-INSERT INTO RegionalCenter(description, location) VALUES
-    ('Centro Universitario Regional del Centro', 'Comayagua'),
-    ('Centro Universitario Regional del Litoral Atlántico', 'La Ceiba, Atlántida'),
-    ('Centro Universitario Regional del Litoral Pacífico', 'Choluteca'),
-    ('Centro Universitario Regional Nor-Oriental', 'Juticalpa, Olancho'),
-    ('Centro Universitario Regional del Occidente', 'Santa Rosa de Copán'),
-    ('CRAED Choluteca', 'Choluteca'),
-    ('CRAED Juticalpa', 'Juticalpa, Olancho'),
-    ('CRAED La Entrada', 'La Entrada, Copán'),
-    ('CRAED Paraíso', 'El Paraíso'),
-    ('CRAED Progreso', 'El Progreso, Yoro'),
-    ('CRAED Siguatepeque', 'Siguatepeque, Comayagua'),
-    ('CRAED Tegucigalpa', 'Tegucigalpa'),
-    ('CRAED Tocoa', 'Tocoa, Colón'),
-    ('Instituto Tecnológico Superior de Tela - UNAH', 'Tegucigalpa'),
-    ('UNAH Tecnológico Danlí', 'Danlí, El Paraíso'),
-    ('UNAH Telecentro Marcala', 'Marcala, La Paz'),
-    ('UNAH Valle de Sula', 'San Pedro Sula, Cortés'),
-    ('UNAH Tecnológico Aguan', 'Olanchito, Yoro'),
-    ('Ciudad Universitaria', 'Tegucigalpa')
+INSERT INTO RegionalCenter(description, location, acronym) VALUES
+    ('Centro Universitario Regional del Centro', 'Comayagua', 'CURNO'),
+    ('Centro Universitario Regional del Litoral Atlántico', 'La Ceiba, Atlántida', 'CURLA'),
+    ('Centro Universitario Regional del Litoral Pacífico', 'Choluteca', 'CURLP'),
+    ('Centro Universitario Regional Nor-Oriental', 'Juticalpa, Olancho', 'CURNO'),
+    ('Centro Universitario Regional del Occidente', 'Santa Rosa de Copán', 'CURO'),
+    ('CRAED Choluteca', 'Choluteca', 'CRAED-C'),
+    ('CRAED Juticalpa', 'Juticalpa, Olancho', 'CRAED-J'),
+    ('CRAED La Entrada', 'La Entrada, Copán', 'CRAED-LE'),
+    ('CRAED Paraíso', 'El Paraíso', 'CRAED P'),
+    ('CRAED Progreso', 'El Progreso, Yoro', 'CRAED PR'),
+    ('CRAED Siguatepeque', 'Siguatepeque, Comayagua', 'CRAED-SGT'),
+    ('CRAED Tegucigalpa', 'Tegucigalpa', 'CRAED-TGU'),
+    ('CRAED Tocoa', 'Tocoa, Colón','CRAED-TCA'),
+    ('Instituto Tecnológico Superior de Tela - UNAH', 'Tegucigalpa', 'ITST'),
+    ('UNAH Tecnológico Danlí', 'Danlí, El Paraíso','TEC-DANLI'),
+    ('UNAH Telecentro Marcala', 'Marcala, La Paz', 'TEL-MARC'),
+    ('UNAH Valle de Sula', 'San Pedro Sula, Cortés', 'VS'),
+    ('UNAH Tecnológico Aguan', 'Olanchito, Yoro','TEC-AGUAN'),
+    ('Ciudad Universitaria', 'Tegucigalpa', 'CU')
 ;
 INSERT INTO AdministrativeType(description) VALUES
     ('SEDP'),
