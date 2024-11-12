7f44aec334b45dd3a81f06814cebb772580d813a
Samantha <samvel200355@gmail.com>
2024-11-12 00:43:49 -0600

    LOgin para el usuario de admisiones

--- src/Login/Login.php
+++ src/Login/Login.php
@@ -43,6 +43,41 @@
             
         }
 
+        /**
+         * author: dorian.contreras@unah.hn
+         * version: 0.1.0
+         * date: 12/11/24
+         */
+        public function loginAdmission(string $user, string $password) : bool{
+
+            //hacer validaciones en el if
+            if(true){
+                $query = "SELECT a.personalEmail, a.password 
+                        FROM Employee a  
+                        INNER JOIN Administrative b
+                        ON a.id = b.id
+                        WHERE a.personalEmail = ? AND b.administrativeType = 2;";
+
+                try{
+                    $result = $this->mysqli->execute_query($query, [$user]);
+                    
+                    foreach($result as $row){
+                        if(password_verify($password, $row["password"])){
+                            return true;
+                        }
+                    }
+
+                    return false;
+                    
+                }catch (Exception $e){
+                    return false;
+                }
+            }else{
+                return false;
+            }
+            
+        }
+
         // Método para cerrar la conexión
         public function closeConnection() {
             $this->mysqli->close();
