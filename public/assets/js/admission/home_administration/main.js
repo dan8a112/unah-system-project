364025fc8bde17d053b9b41e3d809f1c6d2f9dfa
angelcastillo25 <149858015+angelcastillo25@users.noreply.github.com>
2024-11-13 14:18:30 -0600

    funcionalidad para poder entrar a las inscripciones. Se muestran los examenes que hay que hacer una vez terminada la inscripcion.

--- public/assets/js/admission/home_administration/main.js
+++ public/assets/js/admission/home_administration/main.js
@@ -2,53 +2,6 @@ import {BarChart} from './chart.js';
 import { makeCurrentProcesCard, makeTimeLine, separateData } from './Action.js';
 import { HttpRequest } from '../../modules/HttpRequest.js';
 
-const data = {
-    currentProces: {
-        id: 1,
-        name: "II Proceso 2024"
-    },
-    previousProcessesSummary: {
-        applicantsAmount: [5000, 4500, 3000, 5200, 5500, 4830, 4500],
-        processes: ["1 2021", "2 2021", "1 2022", "2 2022", "1 2023", "2 2023", "1 2024"]
-    },
-    previousProcesses: [
-    {
-        year: "2024",
-        processes: [
-            {
-                id: 4,
-                title: "I Proceso 2024"
-            }
-        ]
-    },
-    {
-        year: "2023",
-        processes: [
-            {
-                id: 3,
-                title: "II Proceso 2023"
-            },
-            {
-                id: 2,
-                title: "I Proceso 2023"
-            }
-        ]
-    },
-    {
-        year: "2022",
-        processes: [
-            {
-                id: 1,
-                title: "II Proceso 2022"
-            },
-            {
-                id: 6,
-                title: "I Proceso 2022"
-            }
-        ]
-    }
-    ]
-}
 
 const logoutButton = document.querySelector("button#logoutButton");
 
