41d97b3797667fca766c0f3ed459d1facdb339bb
dan8a112 <danyochoa112@gmail.com>
2024-11-13 14:32:19 -0600

    Cambios en landing y se cambian nombres de tablas en querys sql

--- public/assets/js/loginAPA/main.js
+++ public/assets/js/loginAPA/main.js
@@ -4,5 +4,5 @@ const loginForm = document.querySelector("#loginForm");
 
 //Se envia el formulario de login con la ruta de la api y la ruta a redireccionar
 loginForm.addEventListener("submit", (e)=>{
-    Action.submitLoginForm(e,"../../../api/post/loginAdmission", "../admission/administrative_home.html")
+    Action.submitLoginForm(e,"../../../api/post/loginAdmission", "../admission/administrative_home.php")
 } );
