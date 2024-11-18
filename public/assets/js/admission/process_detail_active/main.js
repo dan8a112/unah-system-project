c50c7d4e3ffcf185b3548a28a2654ae2bcfc613a
angelcastillo25 <149858015+angelcastillo25@users.noreply.github.com>
2024-11-17 22:36:45 -0600

    Se implementa el poder mandar correos desde el frontend

--- public/assets/js/admission/process_detail_active/main.js
+++ public/assets/js/admission/process_detail_active/main.js
@@ -1,9 +1,21 @@
-import { Action } from "./Action.js"
+import { Action } from "./Action.js";
+import { HttpRequest } from "../../modules/HttpRequest.js";
+import { Modal } from "../../modules/Modal.js";
 
-//Se obtiene la información que carga la pagina del detalle de proceso de admision actual
+// Se obtiene la información que carga la página del detalle de proceso de admisión actual
 Action.fetchActiveData();
 
-const downloadCSVButton = document.getElementById("downloadCsvBtn");
+// Selecciona el botón de enviar correos
+const sendEmailsButton = document.getElementById("sendEmailsButton");
+const url = "../../../api/get/admission/sendMails";
 
-//Se agrega accion al boton de descargar csv
-downloadCSVButton.addEventListener("click", Action.downloadCSV);
+// Agrega la acción de enviar correos
+sendEmailsButton.addEventListener("click", async () => {
+    try {
+        const response = await HttpRequest.get(url);
+        console.log("Correos enviados:", response);
+        Modal.closeModal();
+    } catch (error) {
+        console.error("Error al enviar correos:", error);
+    }
+});
