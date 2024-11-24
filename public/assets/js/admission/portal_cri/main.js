8e26b3128abb0ab33f3d2960d9eb05d8597602ef
dan8a112 <danyochoa112@gmail.com>
2024-11-24 00:11:32 -0600

    Comportamiento de prueba portal CRI

---
+++ public/assets/js/admission/portal_cri/main.js
@@ -0,0 +1,65 @@
+import { Action } from "./Action.js"
+
+const data = {
+    period: "Octubre 2024",
+    stats: {
+        dailyGoal: 140,
+        totalReviewed: 345
+    },
+    unreviewedInscriptions : {
+        amountUnreviewed: 2,
+        unreviewedList: [
+            {
+                id: 1,
+                name: "Daniel Alexander Ochoa Osavas",
+                career: "Ingenieria en Sistemas",
+                inscriptionDate: "12/10/2024"
+            },
+            {
+                id: 2,
+                name: "Maria Alejandra Hernandez Alvarado",
+                career: "Ingenieria en Sistemas",
+                inscriptionDate: "12/10/2024"
+            }
+        ]
+    },
+    reviewedInscriptions : {
+        amountReviewed: 2,
+        reviewedList: [
+            {
+                id: 1,
+                name: "Daniel Alexander Ochoa Osavas",
+                career: "Ingenieria en Sistemas",
+                inscriptionDate: "12/10/2024",
+                dictum: "Aprobada"
+            },
+            {
+                id: 2,
+                name: "Maria Alejandra Hernandez Alvarado",
+                career: "Ingenieria en Sistemas",
+                inscriptionDate: "12/10/2024",
+                dictum: "Rechazada"
+            }
+        ]
+    }
+}
+
+const inscriptionData = {
+    applicant: { 
+        name: "Daniel Alexander Ochoa Osavas",
+        dni: "0601-2003-02426",
+        phoneNumber: "98431245",
+        email: "dochoao@unah.hn"
+    },
+    inscription:{
+        options: ["Ingenieria en sistemas", "Ingenieria Civil", "Educacion Fisica"],
+        campus: "Ciudad Universitaria"
+    },
+    schoolCertificate: "FA93C023EACD6",
+}
+
+Action.renderStats(data.stats);
+Action.renderUnreviewed(data.unreviewedInscriptions);
+Action.renderReviewedData(data.reviewedInscriptions);
+
+Action.setInscriptionData(inscriptionData);
