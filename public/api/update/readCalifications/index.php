1e1fe41f557143fa862d6076b5fffdf78759738d
Samantha <samvel200355@gmail.com>
2024-12-10 15:52:35 -0600

    Servicio para leer CSV de las notas de los estudiantes de una seccion

--- public/api/update/readCalifications/index.php
+++ public/api/update/readCalifications/index.php
@@ -4,20 +4,20 @@
      * version: 0.1.0
      * date: 10/12/2024
      * 
-     * Servicio para leer CSV y actualizar las notas de los estudiantes
+     * Servicio para leer CSV y actualizar las notas de los estudiantes de una seccion
      */
     header("Content-Type: application/json");
 
     include_once "../../../../src/DbConnection/DbConnection.php";
-    include_once "../../../../src/Application/Application.php";
+    include_once "../../../../src/Section/Section.php";
 
 
     // Abrir el archivo en modo lectura
-    if(isset($_FILES["pathCsvGrades"])){
+    if(isset($_FILES["pathCsvGrades"]) && isset($_POST["idSection"])){
         $pathCsvGrades = $_FILES['pathCsvGrades']['tmp_name'] ?? '';
         if(file_exists($pathCsvGrades) && !empty($pathCsvGrades)){
-            $dao = new ApplicationDAO(DbConnection::$server, DbConnection::$user, DbConnection::$pass, DbConnection::$dbName);
-            $json = $dao->insertResults($pathCsvGrades);
+            $dao = new SectionDAO(DbConnection::$server, DbConnection::$user, DbConnection::$pass, DbConnection::$dbName);
+            $json = $dao->insertGrades($pathCsvGrades, $_POST["idSection"]);
             
         } else {
             $json = [
