ee940fb72265436730e879b405fa59869a5d38ae
Wilmer <wilmer01morales@gmail.com>
2024-12-10 21:19:02 -0600

    Cambio de nombre en carpeta de API para mayor calidad

--- src/CareerCoordinator/CareerCoordinator.php
+++ src/CareerCoordinator/CareerCoordinator.php
@@ -94,7 +94,77 @@ class CoordinatorDAO {
             "career" => $career
         ];
     }
-
+        /**
+         * author: wamorales@unah.hn
+         * version: 0.2.0
+         * date: 10/12/24
+         * 
+         * Funcion que obtiene la carga academica del un periodo mandado como parametro
+         */
+        public function getAcademicLoadHistoric(int $coordinatorId, int $periodId, int $offset = 0) {
+            // Obtener la lista de periodos
+    
+            // Obtener el periodo mandado como parametro
+            $queryCurrentPeriod = "SELECT AcademicEvent.id id,
+                     CONCAT(AcademicProcess.description, ' ' ,YEAR(AcademicEvent.startDate))  name
+                    FROM AcademicEvent
+                    INNER JOIN AcademicProcess ON AcademicEvent.process=AcademicProcess.id
+                    WHERE AcademicEvent.id=?;";
+    
+            $currentPeriodResult = $this->mysqli->execute_query($queryCurrentPeriod, [$periodId]);
+            $currentPeriod = $currentPeriodResult->fetch_assoc();
+    
+            // Obtener las secciones del periodo actual
+            $querySections = "SELECT Section.id code, Subject.description class, Section.section, 
+                    CONCAT(Employee.names, ' ', Employee.lastNames) professor, 
+                    Section.enrolled , Section.maximumCapacity places, Building.description building, 
+                    Classroom.description classroom
+                    FROM Section
+                    INNER JOIN Subject ON Subject.id=Section.subject
+                    INNER JOIN Professor ON Section.professor=Professor.id
+                    INNER JOIN Employee ON Professor.id=Employee.id
+                    INNER JOIN Classroom ON Section.classroom=Classroom.id
+                    INNER JOIN Building ON Classroom.building=Building.id
+                    INNER JOIN AcademicEvent ON Section.academicEvent=AcademicEvent.id
+                    INNER JOIN Department ON Professor.department=Department.id
+                    WHERE Department.id=(SELECT department FROM Professor WHERE id=?) and AcademicEvent.id=?
+                    LIMIT 10 OFFSET ?;";
+    
+            $sectionListResult = $this->mysqli->execute_query($querySections, [$coordinatorId, $periodId, $offset]);
+            $sectionList = [];
+            while ($row = $sectionListResult->fetch_assoc()) {
+                $sectionList[] = $row;
+            }
+    
+            // Contar el total de secciones
+            $queryAmountSections = "SELECT COUNT(*) AS amount
+            FROM Section
+            INNER JOIN Subject ON Section.subject = Subject.id
+            INNER JOIN Department ON Department.id = Subject.department
+            INNER JOIN AcademicEvent ON Section.academicEvent = AcademicEvent.id
+            WHERE Department.id = (SELECT department FROM Professor WHERE id = ?)
+            AND AcademicEvent.id = ?;";
+    
+            $amountSectionsResult = $this->mysqli->execute_query($queryAmountSections, [$coordinatorId, $periodId]);
+            $amountSections = $amountSectionsResult->fetch_assoc()['amount'];
+    
+            // Obtener la carrera del coordinador
+            $queryCareer = "SELECT Department.description career FROM Professor
+            INNER JOIN Department ON Professor.department=Department.id
+            WHERE Professor.id=?;";
+    
+            $careerResult = $this->mysqli->execute_query($queryCareer, [$coordinatorId]);
+            $career = $careerResult->fetch_assoc()['career'];
+    
+            return [
+                "currentPeriod" => $currentPeriod,
+                "sections" => [
+                    "amountSections" => $amountSections,
+                    "sectionList" => $sectionList
+                ],
+                "career" => $career
+            ];
+        }
     public function closeConnection() {
         $this->mysqli->close();
     }
